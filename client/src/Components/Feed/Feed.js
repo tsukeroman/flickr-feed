fc65b36173c09626a63bdb67a9ac9e5581f3a77c
tsukeroman <tsukeroman@gmail.com>
2018-09-02 15:44:54 +0300

    server initialization + added proxy from client to server

--- client/src/Components/Feed/Feed.js
+++ client/src/Components/Feed/Feed.js
@@ -2,10 +2,49 @@ import React, { Component } from 'react';
 import './Feed.css';
 
 class Feed extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      list: []
+    }
+  }
+
+  componentDidMount() {
+    this.getList();
+  }
+
+  getList = () => {
+    fetch('/api/getList')
+    .then(res => res.json())
+    .then(list => this.setState({ list }))
+  }
+
   render() {
+  	const { list } = this.state;
     return (
-      <div className="Feed">
-      	Feed
+      	<div className="Feed">
+	      	Feed
+	      	<div className="FromServer">
+		      	<h1>List of Items</h1>
+		      	<div>
+			      	{list.length ? (
+			      		<div>
+			      			{list.map((item) => {
+			      				return(
+			      					<div>
+			      						{item}
+			      					</div>
+			      				);
+			      			})}
+			      		</div>
+			      	) : (
+			      		<div>
+			      			<h2>No List Items Found</h2>
+			      		</div>
+			      	)}
+		      	</div>
+	      	</div>
       </div>
     );
   }
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(morgan('tiny'));
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+	const list = ['check1', 'check2', 'check3'];
+	res.json(list);
+	debug('Sent list');
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+app.listen(port, () => {
+	debug(`App is listening on port ${chalk.green(port)}`);
+});
